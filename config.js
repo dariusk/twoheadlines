2ab803afa56f8a3c3ae712aa5934b21b869b6a11
Darius Kazemi <darius.kazemi@gmail.com>
2013-09-27 14:45:59 -0400

    adding empty config file

---
+++ config.js
@@ -0,0 +1,7 @@
+// Put your own Twitter App keys here. See README.md for more detail.
+module.exports = {
+  consumer_key:         'YOUR_CONSUMER_KEY',
+  consumer_secret:      'YOUR_CONSUMER_SECRET',
+  access_token:         'YOUR_ACCESS_TOKEN',
+  access_token_secret:  'YOUR_ACCESS_TOKEN_SECRET'
+};
